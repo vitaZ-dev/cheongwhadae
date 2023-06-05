407f0c917f477d06fb8cb3739b89f0b37fe9070a
vitaZ-dev <vitaminzdev@gmail.com>
2023-06-05 22:00:54 +0900

    feat: 지금까지 한 내용들

---
+++ js/header.js
@@ -0,0 +1,40 @@
+window.addEventListener("load", () => {
+  const $header = document.querySelector(".header");
+  const gnb = document.querySelector(".gnb");
+  const gnbMain = document.querySelector(".gnb .mainmenu");
+  const gnbItems = document.querySelectorAll(".gnb .mainmenu > li");
+
+  const gnbSide = document.querySelector("nav.gnb-side");
+  const gnbOpen = document.querySelector(".gnb-open");
+  const gnbClose = document.querySelector(".gnb-close");
+
+  const active = "is--active";
+
+  gnb.addEventListener("mouseenter", () => {
+    $header.classList.add(active);
+    gnbMain.classList.add(active);
+  });
+  gnb.addEventListener("mouseleave", () => {
+    $header.classList.remove(active);
+    gnbMain.classList.remove(active);
+  });
+
+  gnbItems.forEach((item) => {
+    let target = item.children[1];
+    if (target) {
+      item.addEventListener("mouseenter", () => {
+        target.classList.add(active);
+      });
+      item.addEventListener("mouseleave", () => {
+        target.classList.remove(active);
+      });
+    }
+  });
+
+  gnbOpen.addEventListener("click", () => {
+    gnbSide.classList.add("gnb-side-show");
+  });
+  gnbClose.addEventListener("click", () => {
+    gnbSide.classList.remove("gnb-side-show");
+  });
+});
